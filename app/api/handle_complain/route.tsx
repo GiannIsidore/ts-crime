fe2d26b21bd798f6151773f64b733a4f66241c80
GiannIsidore <giannesidore@gmail.com>
2024-07-09 20:12:09 +0800

    walay i return sa front-end...?

--- app/api/handle_complain/route.tsx
+++ app/api/handle_complain/route.tsx
@@ -1,47 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
+
 interface ComplainDetails {
-  fname: string;
-  mname: string;
-  lname: string;
-  email: string;
-  number: string;
-  respondent: string;
-  respondent_address: string;
-  date_occurence: string;
-  address: string;
-  complaint_type: string;
-  complaint_details: string;
+  name: string;
 }
 
 export async function POST(req: NextRequest) {
-  try {
-    const data: ComplainDetails = await req.json();
-    console.log(data);
+  console.log("nidagan");
 
-    const request = await fetch(
-      "http://localhost/git/ts-crime/app/php/complain.php",
-      {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
+  const { name } = await req.json();
 
-    // const response = await request.text();
-    // console.log(response)
+  console.log(name);
 
-    const { data: userData, error} = await request.json();
-
-    if(userData) {
-      console.log(true);
-      console.log(userData)
-    }
-    else if (error) {
-      console.log(false)
-    }
-  } catch (error) {
-    console.error(error);
-  }
+  return NextResponse.json({ data: "Internal Server Error" }, { status: 500 });
 }
