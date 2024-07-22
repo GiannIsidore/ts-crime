4d68f8772a2f42a34844fbbd8a9e2402794b57d2
GiannIsidore <giannesidore@gmail.com>
2024-07-22 16:33:50 +0800

    basaha ninyo ang SCOPE.jsx mao na atoa checklist 🤙 eyy!!

---
+++ app/SCOPE.jsx
@@ -0,0 +1,22 @@
+//Dashboard Features:
+
+//?Add a Case Explanation: Users can enter detailed explanations for new complaints.
+//ongoing ￣へ￣↗
+
+//?View List of Cases: Display all submitted cases with options to filter, sort, and search.
+
+//?Complainant List: A list of all complainants, including details such as name and contact information.
+
+//?Respondents List: A list of respondents, detailing their information and associated cases.
+
+//?Employee Management: Manage user roles and permissions, including adding and updating employee details.
+
+//!User Management:Role-Based Access: Different access level for employees and administrative staff. (►__◄) dapat lang
+
+//?Case Tracking: Track the status of each case, including submission, review, and resolution stages.
+
+//?Generate Reports: Create reports on case statistics, trends, and other relevant data.
+
+//?Automated Notifications: Send notifications to users about case updates, deadlines, and other important events.
+
+//!Add outsiders and possible new residents . just finished kanina pero naa panay future changes (◕ᴗ◕✿)
